a6ca4e9547c70d30de06a83db3d548c6ecd45d1c
Juane20 <60465454+Juane20@users.noreply.github.com>
2023-11-05 20:49:12 -0300

    Delete OOP directory

--- OOP/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main { //This is a class, a class is a "template" from objects, and objects, are instances from classes.
-    int x = 5;
-    public static void main(String[] args) {
-        Main myObj = new Main();
-        System.out.println(myObj.x); //The object myObj has inherited the x value from the class.
-    }
-}
--- OOP/src/Second.java
+++
@@ -1,6 +0,0 @@
-public class Second {
-    public static void main(String[] args) {
-        Main object = new Main(); //The "Main" refers to the Main.java class.
-        System.out.println(object.x); //Usually, we make all the classes in Main and then use it from another class.
-    }
-}
